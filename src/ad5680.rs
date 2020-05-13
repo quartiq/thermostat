53930497e4a9523ac877538346472d9799fc2ca6
Astro <astro@spaceboyz.net>
2020-05-13 23:19:20 +0200

    ad5680: fix MAX_VALUE

--- src/ad5680.rs
+++ src/ad5680.rs
@@ -15,7 +15,7 @@ pub const SPI_MODE: spi::Mode = spi::Mode {
 /// 30 MHz
 pub const SPI_CLOCK: MegaHertz = MegaHertz(30);
 
-pub const MAX_VALUE: u32 = 0x20000;
+pub const MAX_VALUE: u32 = 0x3FFFF;
 
 /// [AD5680](https://www.analog.com/media/en/technical-documentation/data-sheets/AD5680.pdf) DAC
 pub struct Dac<SPI: Transfer<u8>, S: OutputPin> {
