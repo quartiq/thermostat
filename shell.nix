7a28cb1cd474fea05adc5135af6f69f83e439d96
Astro <sm@m-labs.hk>
2020-12-13 20:42:29 +0100

    shell.nix: use rustPlaform from <nix-scripts> by default
    Fixes Gitea issue #25

--- shell.nix
+++ shell.nix
@@ -1,10 +1,16 @@
-{ mozillaOverlay ? builtins.fetchTarball "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz"
+{ mozillaOverlay ? builtins.fetchTarball "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz",
+  latestRustNightly ? false,
 }:
 let
   pkgs = import <nixpkgs> {
     overlays = [ (import mozillaOverlay) ];
   };
-  rust = pkgs.rustChannelOfTargets "nightly" null [ "thumbv7em-none-eabihf" ];
+  rust =
+    if latestRustNightly
+    then pkgs.rustChannelOfTargets "nightly" null [ "thumbv7em-none-eabihf" ]
+    else (pkgs.recurseIntoAttrs (
+      pkgs.callPackage (import <nix-scripts/stm32/rustPlatform.nix>) {}
+    )).rust.cargo;
 in
 pkgs.mkShell {
   name = "thermostat-env";
