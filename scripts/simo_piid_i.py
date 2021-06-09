0140a2f292880ec7c42f3d74b47f469e3b266eb8
SingularitySurfer <norman_krackow@gmx.de>
2021-06-09 11:57:31 +0200

    lots of testing with siso/simo PIID

---
+++ scripts/simo_piid_i.py
@@ -0,0 +1,69 @@
+import socket
+import time
+from coef import *
+
+''' This script configures the thermostat iir matrix for a single temp input,
+dual TEC output with one PIID controlled, the other with another pure I from the
+output of the first. (aka for very slow changes)'''
+
+target = 45  # temperature target (°C)
+
+# gains in dB, freqs relative to f_sample
+k_i1 = 60  # integral gain 1 (aka gain at DC)
+fc_i1 = 0.0001  # integral gain cutoff 1
+k_i2 = 60  # integral gain 2 (aka gain at DC)
+fc_i2 = 0.0001  # integral gain cutoff 2
+k_p = 16  # proportional gain
+k_d = 30  # derivative gain (aka gain at nyquist)
+fc_d = 0.01  # derivative gain cutoff
+
+
+ba_0 = pi(fc_i1, k_p/2, k_i1)
+ba_1 = pi(fc_i2, 0, k_i2)
+ba_2 = pd(fc_d, k_p/2, k_d)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('192.168.1.26', 23))
+msg = ''
+
+
+s.send('pwm 0 max_v 1.5\n'.encode())
+time.sleep(0.1)
+s.send('pwm 0 max_i_pos 0.8\n'.encode())
+time.sleep(0.1)
+s.send('pwm 0 max_i_neg 0.8\n'.encode())
+time.sleep(0.1)
+s.send('pwm 1 max_v 1.5\n'.encode())
+time.sleep(0.1)
+s.send('pwm 1 max_i_pos 0.8\n'.encode())
+time.sleep(0.1)
+s.send('pwm 1 max_i_neg 0.8\n'.encode())
+time.sleep(0.1)
+s.send('matrix target 0 val {}\n'.format(target).encode())
+time.sleep(0.1)
+s.send('matrix in 0 temp 0\n'.encode())
+time.sleep(0.1)
+s.send('iir 2 {:4.8f} {:4.8f} {:4.8f} {:4.8f} {:4.8f}\n'.format(ba_0[0], ba_0[1], ba_0[2], ba_0[3], ba_0[4]).encode())
+time.sleep(0.1)
+s.send('matrix target 1 matrix 0\n'.encode())  # set target of iir1 to iir0
+time.sleep(0.1)
+s.send('iir 3 {:4.8f} {:4.8f} {:4.8f} {:4.8f} {:4.8f}\n'.format(ba_1[0], ba_1[1], ba_1[2], ba_1[3], ba_1[4]).encode())
+time.sleep(0.1)
+s.send('matrix target 2 matrix 1\n'.encode())  # set target of iir2 to iir1
+time.sleep(0.1)
+s.send('iir 4 {:4.8f} {:4.8f} {:4.8f} {:4.8f} {:4.8f}\n'.format(ba_2[0], ba_2[1], ba_2[2], ba_2[3], ba_2[4]).encode())
+time.sleep(0.1)
+s.send('matrix target 3 matrix 2\n'.encode())  # set target of iir3 to iir2
+time.sleep(0.1)
+s.send('iir 5 0.002 0.002 0 1 0\n'.encode())
+time.sleep(0.1)
+s.send('pwm 0 matrix 3\n'.encode())  # set pwm 0 to PIID output
+time.sleep(0.1)
+s.send('pwm 1 matrix 4\n'.encode())  # set pwm 0 to pure I output
+time.sleep(0.1)
+s.send('report\n'.encode())
+msg = s.recv(1024).decode("utf-8")
+
+print(msg)
+s.close()
--- scripts/stream_2ch.py
+++ scripts/stream_2ch.py
@@ -6,23 +6,25 @@ import datetime as dt
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.1.26', 23))
 
-temp_offset = -45   # for example to display the temp error instead
+temp_offset1 = -45   # for example to display the temp error instead
+temp_offset2 = -45   # for example to display the temp error instead
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 xs = []
 temps1 = []
 currents1 = []
+temps2 = []
+currents2 = []
 
-def animate(i, xs, temps1, currents1):
+def animate(i, xs, temps1, currents1, temps2, currents2):
     s.send('report\n'.encode())
 
     msg = s.recv(1024).decode("utf-8")
 
     m1, m2, m3 = msg.split("\"temperature\":")
     m2, m4 = m2.split(",\"pid_engaged")
-    temp1 = float(m2) + temp_offset
-    print(temp1)
+    temp1 = float(m2) + temp_offset1
 
     m4, m5 = m4.split("\"i_set\":")
     m5, m6 = m5.split(",\"vref\":")
@@ -33,17 +35,38 @@ def animate(i, xs, temps1, currents1):
     if -10 > i_set1:
         i_set1 = -10
 
+    m2, m4 = m3.split(",\"pid_engaged")
+    temp2 = float(m2) + temp_offset2
+    print(temp1, temp2)
+
+    m4, m5 = m4.split("\"i_set\":")
+    m5, m6 = m5.split(",\"vref\":")
+
+    i_set2 = float(m5)
+    if 10 < i_set2:
+        i_set2 = 10
+    if -10 > i_set2:
+        i_set2 = -10
+
     xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
     temps1.append(temp1)
     currents1.append(i_set1)
+
+    temps2.append(temp2)
+    currents2.append(i_set2)
+
+
      # Limit x and y lists to 100 items
     xs = xs[-200:]
     temps1 = temps1[-200:]
     currents1 = currents1[-200:]
+    temps2 = temps2[-200:]
+    currents2 = currents2[-200:]
 
     # Draw x and y lists
     ax.clear()
     ax.plot(xs, temps1, currents1)
+    ax.plot(xs, temps2, currents2)
     ax.grid()
 
     # Format plot
@@ -53,5 +76,5 @@ def animate(i, xs, temps1, currents1):
     plt.ylabel('Temperature (°C) / Current (A)')
 
 # Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs=(xs, temps1, currents1), interval=200)
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, temps1, currents1, temps2, currents2), interval=200)
 plt.show()
